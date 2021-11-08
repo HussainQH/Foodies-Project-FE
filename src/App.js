5a20ac2d857b106ac74a19e3c22d76557f0c808b
Bandar93 <alshalahi93@outlook.com>
2021-11-08 19:23:57 +0300

    DONE

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import authStore from './stores/authStore';
 
 import NavBar from './components/NavBar';
+import userEvent from '@testing-library/user-event';
 function App() {
   return (
     <div className="App">
       <NavBar />
-     <h1>Test</h1>
+     <h1>{authStore.user ? authStore.user.username : "NO USER"}</h1>
     </div>
   );
 }
