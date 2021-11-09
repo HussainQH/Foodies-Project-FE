d023ae7a8dfd5bab9f0285aed1e004f0b6c463fc
Sulaiman <alsalehsulaiman1@gmail.com>
2021-11-09 16:39:06 +0300

    signin-signout

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import './App.css';
 import authStore from './stores/authStore';
 
 import NavBar from './components/NavBar';
-import userEvent from '@testing-library/user-event';
+
 function App() {
 	return (
 		<div className="App">
 			<NavBar />
-			<h1>{authStore.user ? authStore.user.username : 'NO USER'}</h1>
 		</div>
 	);
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,32 +3,36 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 import SignUpModal from "./Signup";
 import SignInModal from "./Signin";
 import authStore from '../stores/authStore';
-
+import Logout from "./Logout";
+import { observer } from "mobx-react";
 
 
 function NavBar() {
   return (
     <Navbar
-    style={{ backgroundColor: "#E67F0D" }}
-    className="navbar"
-    bg="dark"
-    variant="dark"
-  >
-    <Container>
-      <Nav className="me-auto">
-    <p>   {authStore.user ?   authStore.user.username : "welcome guest" } </p>
-       
-       (
-          <>
-          <p>   {authStore.user ?   authStore.user.username : "welcome guest" } </p>
-            <SignUpModal /> 
-            <SignInModal />
-          </>
-        )
-      </Nav>
-    </Container>
-  </Navbar>
+      style={{ backgroundColor: "#E67F0D" }}
+      className="navbar"
+      bg="dark"
+      variant="dark"
+    >
+      <Container>
+        <Nav className="me-auto">
+         <p className="red"> {authStore.user ? (
+            <>
+               Hello {authStore.user.username}
+             
+              <Logout />
+            </>
+          ) : (
+            <>
+              <SignUpModal />
+              <SignInModal />
+            </>
+          )}</p>
+        </Nav>
+      </Container>
+    </Navbar>
 );
 }
 
-export default NavBar;
+export default observer(NavBar);
