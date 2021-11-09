a968641c76d26614c22973ce784a3886756a4a85
HussainQH <hussainqh@gmail.com>
2021-11-09 17:05:27 +0300

    Done With sign in and sign out

--- src/App.js
+++ src/App.js
@@ -1,25 +1,6 @@
-
 import "./App.css";
-
 import CuisineList from "./components/CuisineList";
-
 import NavBar from "./components/NavBar";
-
-
-import authStore from './stores/authStore';
-
-
-import NavBar from './components/NavBar';
-
-function App() {
-	return (
-		<div className="App">
-			<NavBar />
-		</div>
-	);
-
-
-import userEvent from '@testing-library/user-event';
 
 function App() {
   return (
@@ -28,12 +9,8 @@ function App() {
 
       <CuisineList />
       <h1>Test</h1>
-
-     <h1>{authStore.user ? authStore.user.username : "NO USER"}</h1>
-
     </div>
   );
-
 }
 
 export default App;
